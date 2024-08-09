3b916fde40d36fe8d638f11206c04fe19f9c754b
Jing Liu <jintonic@physino.xyz>
2024-08-09 15:53:33 -0500

    moved main.js to /js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+// for libraries installed by npm
 import * as THREE from 'three';
+// for libraries not maintained by npm
+import { CSG } from './libs/CSGMesh.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -9,19 +13,22 @@ document.body.appendChild( renderer.domElement );
 const geo1 = new THREE.BoxGeometry( 1, 1, 1 );
 const mat1 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geo1, mat1 );
-scene.add( cube );
+//scene.add( cube );
 
-const geo2 = new THREE.CylinderGeometry(1,1,1,32,1,false,0,Math.PI/2); 
+const geo2 = new THREE.CylinderGeometry(1,1,1,32,1,false,0, Math.PI); 
 const mat2 = new THREE.MeshBasicMaterial( {color: 0xffff00} ); 
 const cylinder = new THREE.Mesh( geo2, mat2 );
-scene.add( cylinder );
+const aCSG = CSG.fromMesh(cylinder);
+const tube = CSG.toMesh(aCSG, new THREE.Matrix4());
+scene.add(tube);
 
 camera.position.z = 5;
 
 function animate() {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	cylinder.rotation.y += 0.01;
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
+	tube.rotation.x += 0.01;
+	tube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
