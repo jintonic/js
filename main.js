1d0fb94b9db64474a8d3d09539415af2f4b1e27c
Jing Liu <jintonic@physino.xyz>
2024-08-08 22:59:54 -0500

    added a cylinder

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const geo1 = new THREE.BoxGeometry( 1, 1, 1 );
+const mat1 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geo1, mat1 );
 scene.add( cube );
+
+const geo2 = new THREE.CylinderGeometry(1,1,1,32,1,false,0,Math.PI/2); 
+const mat2 = new THREE.MeshBasicMaterial( {color: 0xffff00} ); 
+const cylinder = new THREE.Mesh( geo2, mat2 );
+scene.add( cylinder );
 
 camera.position.z = 5;
 
 function animate() {
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	cylinder.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
