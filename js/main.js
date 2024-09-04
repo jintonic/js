5bec729293d13f25de0f32a1c357eec0c3519670
chitrashensah <chitrashensah@gmail.com>
2024-09-04 14:06:10 -0500

    Added sphere

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ import { CSG } from './libs/CSGMesh.js';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 import { DragControls } from 'three/addons/controls/DragControls.js';
 
+//Adding box
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xaaaaaa );
-
-const geo1 = new THREE.BoxGeometry( 2/2, 1/2, 1/2 );
+var pX=1 ,pY=1,pZ=1;
+const geo1 = new THREE.BoxGeometry( pX*2, pY*2, pZ*2 );
 const mat1 = new THREE.MeshLambertMaterial( { color: 0x00ab12 } );
 const cube = new THREE.Mesh( geo1, mat1 );
 cube.receiveShadow = true;
@@ -17,20 +18,19 @@ cube.position.set(0, 0, -3);
 scene.add( cube );
 
 
-// use Shape and ExtrudeGeometry to create a solid pie
-// Creating the tube with CSG (as it was in the original code)
+// use Shape and ExtrudeGeometry to create a solid tube
 var pie = new THREE.Shape();
 // https://threejs.org/docs/#api/en/extras/core/Path.absarc
-const phi0=Math.PI/3; const dPhi=Math.PI/3;
-const x0=0, y0=0, z0=0, rMin=0.5, rMax=1, dZ=1;
-pie.absarc(x0, y0, rMax, phi0, phi0+dPhi, false);
+var phi0=Math.PI/3; var dPhi=Math.PI/3;
+var x0=0, y0=0, z0=0, pRMin=0.5, pRMax=0.5, pDz=1;
+pie.absarc(x0, y0, pRMax, phi0, phi0+dPhi, false);
 pie.lineTo(0, 0);
 // https://threejs.org/docs/#api/en/geometries/ExtrudeGeometry
-const settingss = { depth: 2*dZ, bevelEnabled: false };
+const settingss = { depth: 2*pDz, bevelEnabled: false };
 const outerTubeGeo = new THREE.ExtrudeGeometry( pie, settingss ); 
-outerTubeGeo.translate(x0,y0,z0-dZ);
+outerTubeGeo.translate(x0,y0,z0-pDz);
 
-const innerTubeGeo = new THREE.CylinderGeometry( rMin, rMin, 2*dZ );
+const innerTubeGeo = new THREE.CylinderGeometry( pRMin, pRMin, 2*pDz );
 innerTubeGeo.rotateX(Math.PI/2);
 innerTubeGeo.translate(x0,y0,z0);
 
@@ -45,7 +45,8 @@ scene.add(tube);
 const tubeHelper = new THREE.BoxHelper(tube, 0xffff00);
 scene.add(tubeHelper);
 
-// Define parameters for the cone (formerly the cylinder)
+
+// Adding cone
 const pRmaxbottom = 1, pRmintop = 0.5, pDzheight = 2
 const outerConeGeo = new THREE.CylinderGeometry(pRmintop, pRmaxbottom,  pDzheight);
 const innerConeGeo = new THREE.CylinderGeometry(pRmintop / 1.5, pRmaxbottom / 1.5,  pDzheight);
@@ -64,16 +65,15 @@ const hollowboxcsg= hollowConeCSG.subtract(boxcsg)
 const cone = CSG.toMesh(hollowboxcsg, new THREE.Matrix4(), mat1);
 cone.receiveShadow = true;
 cone.position.set(0, 0, 3);
+cone.rotation.x=Math.PI/2;
 scene.add(cone);
 
 const coneHelper = new THREE.BoxHelper(cone, 0xffff00);
 scene.add(coneHelper);
 
 
-// Sphere pie shape
-const radius = 1;   
-const startPhi = 0;       
-const deltaPhi = Math.PI/2;  
+// Adding Sphere pie shape
+var radius = 1, startPhi = 0,  deltaPhi = Math.PI/2;  
 
 // Create the pie shape with a larger radius to cut through the entire sphere
 const pie2 = new THREE.Shape();
@@ -102,18 +102,15 @@ sphere.receiveShadow = true;
 sphere.position.set(3, 0, 0);
 
 // Add the resulting mesh to the scene
-scene.add(sphere);
+// scene.add(sphere);
 
 // Optional: Add a BoxHelper to visualize the boundaries
 const sphereHelper = new THREE.BoxHelper(sphere, 0xffff00);
-scene.add(sphereHelper);
+// scene.add(sphereHelper);
 
 
 /// Create the pie shape with a larger radius to cut through the entire sphere
-const pieshape = new THREE.Shape();
-const pieradius = 2;
-const piestartphi = 0;
-const piedeltaphi = Math.PI / 2;
+let pieshape = new THREE.Shape(),  pieradius = 2, piestartphi = 0, piedeltaphi = Math.PI / 2;
 pieshape.absarc(0, 0, pieradius, piestartphi, piestartphi + piedeltaphi, false);
 pieshape.lineTo(0, 0);
 
@@ -149,21 +146,129 @@ const lastcsg = finalmeshcsg.intersect(conecsg); // CSG for pie, sphere, and con
 // Convert the result CSG back to a THREE.Mesh
 const coneremain = CSG.toMesh(lastcsg, new THREE.Matrix4(), mat1);
 coneremain.receiveShadow = true;
-coneremain.position.set(0, 0, 0);
+coneremain.position.set(-3, 0, 3);
 
 // Add the resulting mesh to the scene
-scene.add(coneremain);
+// scene.add(coneremain);
 
 // Optional: Add a BoxHelper to visualize the boundaries
 const coneremainHelper = new THREE.BoxHelper(coneremain, 0xffff00);
-scene.add(coneremainHelper);
+// scene.add(coneremainHelper);
 
+function GetSphere(pRmin, pRmax, pSTheta, pDTheta, pSPhi, pDPhi) {
+
+    const pETheta=pSTheta+pDTheta;  //adding end Theta as it accepts delta phi
+    //create sphere geometry
+    const sphereGeometry = new THREE.SphereGeometry(pRmax);
+    const innerSphereGeometry = new THREE.SphereGeometry(pRmin);
+
+    //Create box geometry for making hemisphere cuts
+    const boxGeometry = new THREE.BoxGeometry(pRmax * 2, pRmax, pRmax * 2);
+    boxGeometry.rotateX(Math.PI/2);
+    boxGeometry.translate(0, 0, pRmax / 2);
+
+    // Create Cone geometries to make a cut
+    const cone1Geometry = new THREE.CylinderGeometry(  pRmax * Math.tan(pSTheta), 0.00001,pRmax); //Smaller upper cone
+    cone1Geometry.rotateX(Math.PI/2);
+    cone1Geometry.translate(0, 0, pRmax / 2);
+
+    const cone2Geometry = new THREE.CylinderGeometry( pRmax * Math.tan(pETheta), 0.0001 , pRmax); //Bigger upper cone
+    cone2Geometry.rotateX(Math.PI/2);
+    cone2Geometry.translate(0,0 ,pRmax / 2);
+
+    const cone3Geometry = new THREE.CylinderGeometry(0.0001,  pRmax * Math.tan(Math.PI-pSTheta), pRmax); //Smaller lower cone
+    cone3Geometry.rotateX(Math.PI/2);
+    cone3Geometry.translate(0, 0,-pRmax / 2);
+
+    const cone4Geometry = new THREE.CylinderGeometry(0.0001, pRmax * Math.tan(Math.PI-pETheta), pRmax); //Bigger lower cone
+    cone4Geometry.rotateX(Math.PI/2);
+    cone4Geometry.translate(0, 0, -pRmax / 2);
+
+    // Create the pie shape 
+    const pieShape = new THREE.Shape();
+    pieShape.absarc(0, 0, pRmax, pSPhi, pSPhi + pDPhi, false);
+    pieShape.lineTo(0, 0);
+    const extrusionsettings = { depth: 2 * pRmax, bevelEnabled: false };
+    const pieGeometry = new THREE.ExtrudeGeometry(pieShape, extrusionsettings);
+    pieGeometry.translate(0, 0, -pRmax);
+
+    // Convert geometries to CSG objects
+    const sphereCSG = CSG.fromGeometry(sphereGeometry);  
+    const innerSphereCSG = CSG.fromGeometry(innerSphereGeometry);
+    const cone1CSG = CSG.fromGeometry(cone1Geometry);
+    const cone2CSG = CSG.fromGeometry(cone2Geometry);
+    const cone3CSG = CSG.fromGeometry(cone3Geometry);
+    const cone4CSG = CSG.fromGeometry(cone4Geometry);
+    const boxCSG = CSG.fromGeometry(boxGeometry);
+    const pieCSG = CSG.fromGeometry(pieGeometry);
+
+    let resultCSG;
+    //get shapes under different conditions
+    if ((pSTheta === 0) && (pETheta > 0 && pETheta < Math.PI / 2)) {  
+        resultCSG = cone2CSG.intersect(sphereCSG);
+
+    } else if ((pSTheta === 0) && (pETheta === Math.PI / 2)) {
+        resultCSG = boxCSG.intersect(sphereCSG);
+
+    } else if ((pSTheta === 0) && (pETheta > Math.PI / 2 && pETheta < Math.PI)) { 
+        resultCSG = sphereCSG.subtract(cone4CSG);
+
+    } else if ((pSTheta > 0 && pSTheta < Math.PI / 2) && (pETheta > pSTheta && pETheta < Math.PI / 2)) { 
+        var step1CSG = cone2CSG.subtract(cone1CSG);
+        resultCSG = step1CSG.intersect(sphereCSG);
+
+    } else if ((pSTheta > 0 && pSTheta < Math.PI / 2) && (pETheta === Math.PI / 2)) { 
+        var step1CSG = boxCSG.subtract(cone1CSG);
+        resultCSG = step1CSG.intersect(sphereCSG);
+
+    } else if ((pSTheta > 0 && pSTheta < Math.PI / 2) && (pETheta > Math.PI / 2 && pETheta < Math.PI)) { 
+        var step1CSG = sphereCSG.subtract(cone1CSG);
+        resultCSG = step1CSG.subtract(cone4CSG);
+
+    } else if ((pSTheta > 0 && pSTheta < Math.PI / 2) && (pETheta === Math.PI)) { 
+        resultCSG = sphereCSG.subtract(cone1CSG);
+
+    } else if ((pSTheta === Math.PI / 2) && (pETheta > Math.PI / 2 && pETheta < Math.PI)) { 
+        var step1CSG = sphereCSG.subtract(boxCSG);
+        resultCSG = step1CSG.subtract(cone4CSG);
+
+    } else if ((pSTheta === Math.PI / 2) && (pETheta === Math.PI)) { 
+        resultCSG = sphereCSG.subtract(boxCSG);
+
+    } else if ((pSTheta > Math.PI / 2 && pSTheta < Math.PI) && (pETheta > pSTheta && pETheta < Math.PI)) { //test
+        step1CSG = cone3CSG.subtract(cone4CSG);
+        resultCSG = step1CSG.intersect(sphereCSG);
+
+    } else if ((pSTheta > Math.PI / 2 && pSTheta < Math.PI) && (pETheta === Math.PI)) { 
+        resultCSG = sphereCSG.intersect(cone3CSG);
+    }
+
+    if (pDPhi<Math.PI*2){                       //make a cut when Phi is less than 360
+        resultCSG = resultCSG.intersect(pieCSG);
+    }
+    if (pRmin>0){
+        resultCSG = resultCSG.subtract(innerSphereCSG);
+    }
+    if (resultCSG) {
+        
+        const spheree = CSG.toMesh(resultCSG, new THREE.Matrix4(), mat1);
+        return spheree;
+    }
+
+}
+
+// Call the function and add the resulting mesh to the scene
+const Sphere = GetSphere(0, 2, Math.PI/4, Math.PI/1.5 , Math.PI/4, Math.PI/1.5 ); //pRmin, pRmax, pSPhi, pDPhi, pSTheta, pDTheta
+scene.add(Sphere);
+
+const SphereHelper = new THREE.BoxHelper(Sphere, 0xffff00);
+scene.add(SphereHelper);
 
 // Adding grid and axes helpers
 const gridHelper = new THREE.GridHelper( 10, 10 );
 scene.add( gridHelper );
 
-// https://threejs.org/docs/#api/en/helpers/AxesHelper
+// // https://threejs.org/docs/#api/en/helpers/AxesHelper
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
 
@@ -195,7 +300,7 @@ controls.enableRotate = true; // Disable camera rotation
 controls.enableZoom = true; // Allow zooming
 
 // https://threejs.org/docs/#examples/en/controls/DragControls
-const drag = new DragControls([cube, tube, cone, sphere, coneremain], camera, renderer.domElement);
+const drag = new DragControls([cube, tube, cone,  Sphere], camera, renderer.domElement);
 
 // Disable MapControls while dragging objects
 drag.addEventListener('dragstart', function () {
@@ -212,8 +317,9 @@ drag.addEventListener('drag', function () {
 
     tubeHelper.update();
     coneHelper.update();
-    sphereHelper.update();
-    coneremainHelper.update();
+    // sphereHelper.update();
+    // coneremainHelper.update();
+    SphereHelper.update();
 });
 
 // Animation loop
